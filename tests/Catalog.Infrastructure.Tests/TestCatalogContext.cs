2c38497f9c2c7ffa36b908e11f6eeee0c31f0c69
AKINTOBA <samuelakinwole@gmail.com>
2025-06-26 22:39:59 +0100

    CRUD Unit Testing for Items

--- tests/Catalog.Infrastructure.Tests/TestCatalogContext.cs
+++ tests/Catalog.Infrastructure.Tests/TestCatalogContext.cs
@@ -1,12 +1,24 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Catalog.Domain.Entities;
+using Catalog.Infrastructure.Tests.Extensions;
+using Microsoft.EntityFrameworkCore;
 
 namespace Catalog.Infrastructure.Tests
 {
-    public class TestCatalogContext
+    public class TestCatalogContext : CatalogContext
     {
-        
+        public TestCatalogContext(DbContextOptions<CatalogContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Seed<Item>("Data/item.json")
+                        .Seed<Artist>("Data/artist.json")
+                        .Seed<Genre>("Data/genre.json");
+
+        }
     }
 }
